0a1eec97f7635c1a6b08ec45fb080a0c8434c209
manickmanick <manickaperumal22@gmail.com>
2026-06-09 21:15:08 +0530

    check analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Manick Portfolio",
@@ -18,7 +17,7 @@ export default function RootLayout({
       <body>
         {children}
 
-        <Analytics />
+        
       </body>
     </html>
   );
